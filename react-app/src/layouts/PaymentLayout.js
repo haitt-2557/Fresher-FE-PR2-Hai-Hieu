8ab6b0318f6334a3b93c0d043276b72836972e72
haitt-2557 <truong.thanh.hai@sun-asterisk.com>
2021-08-12 10:16:14 +0700

    complete task 8 shopping cart

--- react-app/src/layouts/PaymentLayout.js
+++ react-app/src/layouts/PaymentLayout.js
@@ -1,9 +1,17 @@
 /** @format */
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
-function PrivateLayout({ component: Component, ...props }) {
+import { toast } from 'react-toastify';
+function PaymentLayout({ component: Component, ...props }) {
 	const users = localStorage.getItem('profile');
-	return users ? <Route {...props} render={(routerProps) => <Component {...routerProps} />} /> : <Redirect to='/login' />;
+	return users ? (
+		<Route {...props} render={(routerProps) => <Component {...routerProps} />} />
+	) : (
+		<Redirect to='/login'>
+			{toast.warning('You need to log in first !', {
+				position: toast.POSITION.TOP_RIGHT,
+			})}
+		</Redirect>
+	);
 }
-export default PrivateLayout;
+export default PaymentLayout;
--- react-app/src/layouts/PrivateLayouts.js
+++ react-app/src/layouts/PrivateLayouts.js
@@ -2,9 +2,17 @@
 
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
+import { toast } from 'react-toastify';
 function PrivateLayout({ component: Component, ...props }) {
 	const users = localStorage.getItem('profile');
-	return users?.user.role === 'admin' ? <Route {...props} render={(routerProps) => <Component {...routerProps} />} /> : <Redirect to='/' />;
+	return users?.user.role === 'admin' ? (
+		<Route {...props} render={(routerProps) => <Component {...routerProps} />} />
+	) : (
+		<Redirect to='/'>
+			{toast.warning("You don't have permission to access this page", {
+				position: toast.POSITION.TOP_RIGHT,
+			})}
+		</Redirect>
+	);
 }
 export default PrivateLayout;
